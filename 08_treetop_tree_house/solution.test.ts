1860226e65d2b755b05a70366d239bcbc6607000
Atte Moisio <atte.moisio@attemoisio.fi>
2022-12-11 12:34:36 +0200

    Add empty implementation for day 8

---
+++ 08_treetop_tree_house/solution.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "@jest/globals";
+
+describe("Day 8", () => {
+  test("", () => {
+    expect(true).toBeTruthy();
+  });
+});
---
+++ 08_treetop_tree_house/solution.ts
@@ -0,0 +1,7 @@
+export function part1(inputLines: string[]): void {
+  console.log("part1 not implemented yet!");
+}
+
+export function part2(inputLines: string[]): void {
+  console.log("part2 not implemented yet");
+}
